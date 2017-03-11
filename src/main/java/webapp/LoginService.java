093d20a8ae5f0f99f9bdb553f534b35d0fa8425a
QletApps <abhisheksliam@gmail.com>
2017-03-11 23:11:42 +0530

    list todos

--- src/main/java/webapp/LoginService.java
+++ src/main/java/webapp/LoginService.java
@@ -2,7 +2,7 @@ package webapp;
 
 public class LoginService {
     public boolean validateUser(String user, String password) {
-        return user.equalsIgnoreCase("in28Minutes") && password.equals("dummy");
+        return user.equalsIgnoreCase("test") && password.equals("test");
     }
 
 }
